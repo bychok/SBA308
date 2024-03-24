fc3d22c18e643aa4a1e80e1bed2e88f786dadc93
Vadim J. Bychok <vadim@bychok.net>
2024-03-23 19:02:43 -0700

    Removes check verification

--- app.js
+++ app.js
@@ -160,19 +160,19 @@ const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
 console.log(result);
 
-const result2 = [
-  {
-    id: 125,
-    avg: 0.985, // (47 + 150) / (50 + 150)
-    1: 0.94, // 47 / 50
-    2: 1.0, // 150 / 150
-  },
-  {
-    id: 132,
-    avg: 0.82, // (39 + 125) / (50 + 150)
-    1: 0.78, // 39 / 50
-    2: 0.833, // late: (140 - 15) / 150
-  },
-];
+// const check = [
+//   {
+//     id: 125,
+//     avg: 0.985, // (47 + 150) / (50 + 150)
+//     1: 0.94, // 47 / 50
+//     2: 1.0, // 150 / 150
+//   },
+//   {
+//     id: 132,
+//     avg: 0.82, // (39 + 125) / (50 + 150)
+//     1: 0.78, // 39 / 50
+//     2: 0.833, // late: (140 - 15) / 150
+//   },
+// ];
 
-console.log(result2);
+// console.log(check);
